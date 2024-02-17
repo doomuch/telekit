923e24349f00c3038151a30431423299c1a15c90
cσgscídєs <50235526+cogscides@users.noreply.github.com>
2024-02-17 02:10:58 +0200

    create dir `data` for `clients.json`

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,28 @@
 # Use the official image as a parent image
-FROM python:3.8
+FROM python:3.9
 
 # Set work directory
 WORKDIR /app
 
 # Install system dependencies
 RUN apt-get update && apt-get install -y \
-    python3-dev build-essential libssl-dev libffi-dev
+    python3-dev build-essential libssl-dev libffi-dev ffmpeg
 
 # Copy the current directory contents into the container at /app
 COPY . /app
 
+# Create necessary directories
+RUN mkdir -p /app/data
+
 # Install Python dependencies
 RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install python-dotenv
+RUN pip install pydub
 
 # Make port 80 available to the world outside this container
 EXPOSE 80
 
-# Define environment variable
+# Define environment variables
 ENV NAME OPENAI_API_KEY
 ENV NAME API_ID
 ENV NAME API_HASH
--- app.py
+++ app.py
@@ -20,11 +20,16 @@ import typer
 
 app = typer.Typer()
 
-if not os.path.exists('data/clients.json'):
-    with open('data/clients.json', 'w') as f:
+# Ensure the 'data' directory exists
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+clients_file_path = os.path.join(data_dir, 'clients.json') # path/to/clients.json
+
+if not os.path.exists(clients_file_path):
+    with open(clients_file_path, 'w') as f:
         json.dump({}, f)
 
-with open('data/clients.json', 'r') as f:
+with open(clients_file_path, 'r') as f:
     client_data = json.load(f)
 
 
@@ -42,7 +47,7 @@ def add_client(session: str):
         }
     client_data.append(new_client)
     # save json
-    with open('data/clients.json', 'w') as f:
+    with open(clients_file_path, 'w') as f:
         json.dump(client_data, f, indent=4)
 
     print(f"Added new client with session name: {session}")
@@ -57,7 +62,7 @@ def delete_client():
     client_data[:] = [client for client in client_data if client.get("session_name") != session]
     print(client_data)
 
-    with open('data/clients.json', 'w') as f:
+    with open(clients_file_path, 'w') as f:
         json.dump(client_data, f, indent=4)
     print(f"Deleted client with session name: {session}")
 
